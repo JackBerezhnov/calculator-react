c7653182b83d595dec0220f1a08cb94185076863
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2021-03-21 14:04:26 -0700

    deleted operators and numbers components

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -1,11 +1,8 @@
 import React, { useState } from 'react';
 import Result from '../result/Result';
-import NumbersButton from '../numbers/NumbersButton';
-import Operators from '../operators/Operators';
 import ButtonDisplay from '../buttonDisplay/ButtonDisplay';
 
 const Calculator = () => {
-    const [count, setCount] = useState(0);
     const [result, setResult] = useState(0);
 
     function onClick(e) {
@@ -16,12 +13,7 @@ const Calculator = () => {
         <div>
             <h1>Calculator</h1>
             <Result result={result}/>
-            <NumbersButton/>
-            <Operators/>
             <ButtonDisplay onClick={onClick}/>
-            <p>You clicked {count} times</p>
-            <button onClick={() => setCount(count+1)}>Click me!</button>
-            <button onClick={() => setCount(0)}>Clear</button>
         </div>
     );
 }
